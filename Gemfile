01cab16512e70340bfb301eba7e97bafb4b1bc36
Tomohisa Uchida <uddystyle@gmail.com>
2017-10-15 14:04:08 +0900

    Implemented facebook login

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
 gem 'devise'
 gem 'toastr-rails'
+gem 'omniauth-facebook'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root :to => 'pages#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
+
+
+  resources :users, only: [:show]
 end
